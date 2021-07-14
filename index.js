ebec21413d136d589ece8bb91e116ed702a929a7
kenyan-coder <scamsours@gmail.com>
2021-07-14 13:53:43 +0300

    render entered urls

--- index.js
+++ index.js
@@ -2,9 +2,29 @@
 const inputBtn = document.getElementById("input-btn");
 let myLeads =[]
 const inputEl = document.getElementById("input-el")
+// Grab the unordered list element from the DOM
+const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click",function(){
-    console.log("Button clicked from event listener" )
-    myLeads.push("www.awesomelead.com")
-    console.log(myLeads)
+    myLeads.push(inputEl.value)
+    inputEl.value=""
+    renderLeads()
+    
 })
+
+function renderLeads(){
+    listItems=""
+
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += 
+        `
+        <li>
+            <a target='_blank' href='${myLeads[i]}'>
+            ${myLeads[i]}
+            </a>
+        </li>
+        `
+        
+    }
+    ulEl.innerHTML = listItems 
+}
